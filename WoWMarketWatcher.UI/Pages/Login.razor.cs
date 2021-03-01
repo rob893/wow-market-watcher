9782e27b5627a90812f61a6d5908f608cdf14770
Robert Herber <rwherber@gmail.com>
2021-02-28 17:34:52 -0800

    chore: split login file

---
+++ WoWMarketWatcher.UI/Pages/Login.razor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using WoWMarketWatcher.UI.Services;
+
+namespace WoWMarketWatcher.UI.Pages
+{
+    public partial class Login
+    {
+        [Inject]
+        private IAuthService AuthService { get; set; }
+
+        [Inject]
+        private TestService TestService { get; set; }
+
+        [Inject]
+        private ILogger<Login> Logger { get; set; }
+
+        private string Username { get; set; }
+
+        private string Password { get; set; }
+
+        public async Task LoginAsync()
+        {
+            this.Logger.LogInformation("Logging in!");
+
+            var res = await this.AuthService.Login(this.Username, this.Password);
+
+            this.Logger.LogInformation(res.User.UserName);
+        }
+
+        public async Task ClearLoginForm()
+        {
+            await this.TestService.Test();
+            this.Username = "";
+            this.Password = "";
+        }
+    }
+}
