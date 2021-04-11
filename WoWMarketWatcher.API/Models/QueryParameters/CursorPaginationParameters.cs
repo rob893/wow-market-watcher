822474e8b58649a4edb09b7bc285309ed21a2c71
Robert Herber <rwherber@gmail.com>
2021-04-10 20:38:26 -0700

    chore: update validation

--- WoWMarketWatcher.API/Models/QueryParameters/CursorPaginationParameters.cs
+++ WoWMarketWatcher.API/Models/QueryParameters/CursorPaginationParameters.cs
@@ -1,13 +1,47 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace WoWMarketWatcher.API.Models.QueryParameters
 {
-    public record CursorPaginationParameters
+    public record CursorPaginationParameters : IValidatableObject
     {
+        [Range(1, int.MaxValue)]
         public int? First { get; init; }
         public string? After { get; init; }
+        [Range(1, int.MaxValue)]
         public int? Last { get; init; }
         public string? Before { get; init; }
         public bool IncludeTotal { get; init; } = false;
         public bool IncludeNodes { get; init; } = true;
         public bool IncludeEdges { get; init; } = true;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.First != null && this.Last != null)
+            {
+                yield return new ValidationResult("Passing both `first` and `last` to paginate is not supported.", new[] { nameof(this.First), nameof(this.Last) });
+            }
+
+            if (this.After != null && this.First == null && this.Last == null)
+            {
+                yield return new ValidationResult(
+                    $"Either `{nameof(this.First)}` or `{nameof(this.Last)}` is required when passing `{nameof(this.After)}`.",
+                    new[] { nameof(this.After) });
+            }
+
+            if (this.Before != null && this.First == null && this.Last == null)
+            {
+                yield return new ValidationResult(
+                    $"Either `{nameof(this.First)}` or `{nameof(this.Last)}` is required when passing `{nameof(this.Before)}`.",
+                    new[] { nameof(this.Before) });
+            }
+
+            if (!this.IncludeEdges && !this.IncludeNodes)
+            {
+                yield return new ValidationResult(
+                    $"Both `{nameof(this.IncludeEdges)}` and `{nameof(this.IncludeNodes)}` cannot be false.",
+                    new[] { nameof(this.IncludeEdges), nameof(this.IncludeNodes) });
+            }
+        }
     }
 }
